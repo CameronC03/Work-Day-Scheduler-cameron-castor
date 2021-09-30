69a18d706b3fafd34e2e0c6c7934809955663f6d
CameronC <cameroncastor05@gmail.com>
2021-09-30 17:26:40 -0600

    added some vars

---
+++ script.js
@@ -0,0 +1,5 @@
+var timeBlock = $(".container");
+var timeFrame = moment().format(`MMMM Do YYYY`)
+var timeSection = $("#currentDay")
+timeSection.append(timeDisplay)
+var saveBtn = document.querySelectorAll("button")
