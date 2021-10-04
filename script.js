012ae6b278b8913e935af38ea634da5b9eaa2bb9
CameronC <cameroncastor05@gmail.com>
2021-10-04 13:48:12 -0600

    added a function

--- script.js
+++ script.js
@@ -47,6 +47,14 @@ for (var i = 0; i < timesList.length; i++) {
 localStorageFunction();
 
 function localStorageFunction() {
-    
+    $("textarea")[0].value = localStorage.getItem("textarea1")
+    $("textarea")[1].value = localStorage.getItem("textarea2")
+    $("textarea")[2].value = localStorage.getItem("textarea3")
+    $("textarea")[3].value = localStorage.getItem("textarea4")
+    $("textarea")[4].value = localStorage.getItem("textarea5")
+    $("textarea")[5].value = localStorage.getItem("textarea6")
+    $("textarea")[6].value = localStorage.getItem("textarea7")
+    $("textarea")[7].value = localStorage.getItem("textarea8")
+    $("textarea")[8].value = localStorage.getItem("textarea9")
 }
 
