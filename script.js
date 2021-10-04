e7aa0954f26777ce0e52aa11628be160d79d9036
CameronC <cameroncastor05@gmail.com>
2021-10-04 11:50:12 -0600

    added more times

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ var timeFrame = moment().format(`MMMM Do YYYY`)
 var timeSection = $("#currentDay")
 timeSection.append(timeDisplay)
 var saveBtn = document.querySelectorAll("button")
+
+var timeList = [
+    "9 AM",
+    "10 AM",
+    "11 AM",
+    "12 AM",
+    "1 PM",
+    "2 PM",
+    "3 PM",
+    "4 PM",
+    "5 PM",
+
+];
