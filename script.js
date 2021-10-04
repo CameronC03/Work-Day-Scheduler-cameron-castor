783e685e3eabea62c0ef794f1f15baa8a8cad64c
CameronC <cameroncastor05@gmail.com>
2021-10-04 13:44:40 -0600

    adding a function for local storage

--- script.js
+++ script.js
@@ -29,5 +29,24 @@ var times = [
 ];
 
 for (var i = 0; i < timesList.length; i++) {
+    var row1 = $("<div class=`row time-block`>").attr("id", times[i]);
+    var div1 = $("<div class=`hour col-1'>")
+    var div2 = $("<textarea class=`col-10>");
+    var button1 = $("<button type=`button` class=saveBtn col-1 far fa-save`>");
+
+    timeBlock.append(row1)
+    div1.text(timesList[i]);
+    row1.append(div1);
+    div2.text();
+    row1.append(div2)
+    button1.text();
+    row1.append(button1)
+
+}
+
+localStorageFunction();
+
+function localStorageFunction() {
+    
 }
 
